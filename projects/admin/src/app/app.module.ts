ad3a2aff7da4f8a3d0cc2f5d5e02de763411ad16
3aziizz <abdulazeez.thajeel@gmail.com>
2023-10-05 05:41:34 +0300

    laaaa

--- projects/admin/src/app/app.module.ts
+++ projects/admin/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MaterialModule } from './material/material.module';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxSpinnerModule } from "ngx-spinner";
 
 @NgModule({
   declarations: [
@@ -18,10 +19,11 @@ import { HttpClientModule } from '@angular/common/http';
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
+    
     ToastrModule.forRoot(),
     HttpClientModule,
     AppRoutingModule,
-    
+    NgxSpinnerModule.forRoot({ type: 'ball-scale-multiple' }),
     BrowserAnimationsModule
   ],
   providers: [],
--- projects/admin/src/app/auth/components/login/login.component.ts
+++ projects/admin/src/app/auth/components/login/login.component.ts
@@ -4,7 +4,7 @@ import { Login } from '../../context/DTOs';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
-
+import { NgxSpinnerService } from "ngx-spinner";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -15,7 +15,8 @@ export class LoginComponent implements OnInit {
   constructor(private fb:FormBuilder, 
     private service:LoginService,
     private toaster:ToastrService,
-    private router:Router) { }
+    private router:Router,
+    private spinner: NgxSpinnerService) { }
 
   loginForm!:FormGroup
   ngOnInit(): void {
@@ -33,10 +34,12 @@ export class LoginComponent implements OnInit {
   }
 
   login(){
-    this.service.login(this.loginForm.value).subscribe(res=>{
 
+    this.spinner.show();
+    this.service.login(this.loginForm.value).subscribe(res=>{
       this.toaster.success('success','Login success')
       this.router.navigate(['/tasks'])
+      this.spinner.show();
     },error=>{
 
       this.toaster.error(error.error);
